00e9c2e1e5cb9fc83800d520cf286566fa4ef498
MouTurE <m.turkalp53@gmail.com>
2025-01-30 13:33:44 +0300

    State variables added for categories and tasks

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -1,27 +1,39 @@
+import { useState } from "react";
 import TaskItem from "./TaskItem";
 
 interface Props {
-
-    categoryName : string;
-    Tasks : string[];
-    
+  categoryName: string;
+  Tasks: string[];
 }
 
+function CategoryItem({ categoryName, Tasks }: Props) {
+  const [completedTask, setCompletedTask] = useState(1);
 
+  const decrease = () => {
+    setCompletedTask(completedTask - 1);
+    console.log("Completed task:", completedTask);
+  };
 
-function CategoryItem ({categoryName,Tasks}:Props)  {
+  const increase = () => {
+    setCompletedTask(completedTask + 1);
+    console.log("Completed task:", completedTask);
+  };
 
-
-    
-    return (
-
-        <div className="category-task-holder"  id={categoryName}> {Tasks.map((Task,index) => (
-            <>
-            <TaskItem completed={false} key={`${categoryName} Task ${index}`} >{Task}</TaskItem>
-            <TaskItem completed={true} key={`${categoryName} Task ${index}`} >{Task}</TaskItem> 
-            </>
-        ))}  </div>
-    );
+  return (
+    <div className="category-task-holder" id={categoryName}>
+      {" "}
+      {Tasks.map((Task, index) => (
+        <TaskItem
+          taskID={`${categoryName} - Task ${index}`}
+          key={`${categoryName} - Task ${index}`}
+          taskChangedToCompleted={increase}
+          taskChangedToDefault={decrease}
+        >
+          {Task}
+        </TaskItem>
+      ))}{" "}
+    </div>
+  );
 }
 
 export default CategoryItem;
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,29 +1,59 @@
+import { useState } from "react";
 
 interface Props {
-    children: string;
-    completed: boolean;
-  }
-
-
-const completedText = {
-
-  color: "gray",
+  children: string;
+  taskID: string;
+  taskChangedToCompleted: () => void;
+  taskChangedToDefault: () => void;
 }
 
-function TaskItem ({children,completed}:Props) {
-  
-  const uncompletedTask = (
-    <li className = {"tasks-list-item"}><div>{children}</div> </li>
+const completedText = {
+  color: "gray",
+};
+
+function TaskItem({
+  children,
+  taskID,
+  taskChangedToCompleted,
+  taskChangedToDefault,
+}: Props) {
+  const [completed, toggleCompleted] = useState(false);
+
+  const toggleState = () => {
+    toggleCompleted(!completed);
+    if (completed) {
+      taskChangedToCompleted();
+    } else {
+      taskChangedToDefault();
+    }
+  };
+
+  const checkBox = (
+    <div
+      onClick={toggleState}
+      className={
+        completed === false ? "task-checkbox" : "task-checkbox-completed"
+      }
+    ></div>
   );
 
+  const uncompletedTask = (
+    <li className={"tasks-list-item"}>
+      {checkBox} <div>{children}</div>{" "}
+    </li>
+  );
 
   const completedTask = (
-    <li className = {"tasks-list-item"}><div> <s style={completedText}>{children}</s> </div> </li>
+    <li className={"tasks-list-item"}>
+      {checkBox}
+      <div>
+        {" "}
+        <s style={completedText}>{children}</s>{" "}
+      </div>{" "}
+    </li>
   );
 
-  return <>
-    {completed == true ? completedTask : uncompletedTask}
-  </>
+  return <>{completed == true ? completedTask : uncompletedTask}</>;
 }
 
 export default TaskItem;
